9d1ce76fadf1ec34237c5b4c07c80b3d7b1afc6f
shiham101 <shiham.wang@gmail.com>
2015-07-20 21:04:57 +0800

    Update WebLogicPasswordDecryptor.java

--- WebLogicPasswordDecryptor.java
+++ WebLogicPasswordDecryptor.java
@@ -79,7 +79,9 @@ public class WebLogicPasswordDecryptor {
         System.arraycopy(encryptedPassword1, 0, iv, 0, 16);
         byte[] encryptedPassword2 = new byte[16];
         System.arraycopy(encryptedPassword1, 16, encryptedPassword2, 0, 16);
-
+        int encryptedPasswordlength = encryptedPassword1.length - 16 ;
+        byte[] encryptedPassword2 = new byte[encryptedPasswordlength];
+        System.arraycopy(encryptedPassword1, 16, encryptedPassword2, 0, encryptedPasswordlength);
         IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
         Cipher outCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
         outCipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
